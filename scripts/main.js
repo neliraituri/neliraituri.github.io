5d73c0e51374392d7cb40d8014a3ee9af6039512
neliraituri <63425079+neliraituri@users.noreply.github.com>
2020-04-11 20:25:36 +0100

    Image fixing

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/winnie.png') {
+    if(mySrc === 'images/winnie.jpg') {
       myImage.setAttribute ('src','images/winnie2.jpg');
     } else {
-      myImage.setAttribute ('src','images/winnie.png');
+      myImage.setAttribute ('src','images/winnie.jpg');
     }
 }
